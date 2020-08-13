22f428534e17c990bcb50ab608a46da967ede276
Igor Kalinkin <kalinkin92@gmail.com>
2020-08-13 11:09:03 +0300

    FEND-1003 Fix

--- leda/src/DateTimeInput/helpers.ts
+++ leda/src/DateTimeInput/helpers.ts
@@ -172,7 +172,7 @@ const normilizeNumber = (value: number, rules: TimeLimits): number => {
   return value;
 };
 
-export const validateTimeLimits = (timeLimits: TimeLimits): TimeLimits | undefined => {
+export const validateTimeLimits = (timeLimits: TimeLimits | undefined): TimeLimits | undefined => {
   if (!timeLimits) return undefined;
   const [hours, minutes] = timeLimits;
   return [normilizeNumber(hours, HOURS_LIMITS as TimeLimits), normilizeNumber(minutes, MINUTES_LIMITS as TimeLimits)];
