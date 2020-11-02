d4c01e5d336b11503db51db1d64f1d202b4421f6
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-11-02 10:49:02 +0300

    FEND-1153 [MaskedInput] Added minimal demo example

--- demo/components/MaskedInput/index.tsx
+++ demo/components/MaskedInput/index.tsx
@@ -4,12 +4,14 @@ import { BasicUsage } from './BasicUsage';
 import { Controlled } from './Controlled';
 import { Customization } from './Customization';
 import { Validation } from './Validation';
+import { Minimal } from './Minimal';
 
 export const MaskedInput = (): React.ReactElement => (
   <Story title="MaskedInput">
-    <BasicUsage title="Простейшее использование" />
-    <Controlled title="Контролируемый режим" />
-    <Validation title="Валидация" />
-    <Customization title="Кастомизация" />
+    <BasicUsage title="Basic example" />
+    <Controlled title="Controlled mode" />
+    <Validation title="Validation" />
+    <Customization title="Customization" />
+    <Minimal title="Minimal example" />
   </Story>
 );
