a8cc5c67434cc33a4c5424fea82b8fd575ab73f4
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-11-03 14:05:17 +0300

    FEND-1153 [AutoComplete] Added eslint-disable-next-line

--- demo/components/AutoComplete/Minimal.tsx
+++ demo/components/AutoComplete/Minimal.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as L from '../../../leda';
 
+// eslint-disable-next-line
 export const Minimal = (props: any): React.ReactElement => {
   const [value, setValue] = React.useState('');
 
