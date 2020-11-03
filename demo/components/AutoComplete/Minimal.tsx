0b02b5c14a1626e023971210a7fc62dc690b5d38
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-11-03 12:57:36 +0300

    FEND-1153 [AutoComplete] Added minimal demo example

---
+++ demo/components/AutoComplete/Minimal.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as L from '../../../leda';
+
+export const Minimal = (props: any): React.ReactElement => {
+  const [value, setValue] = React.useState('');
+
+  return (
+    <L.Div>
+      <L.AutoComplete
+        data={[
+          'London',
+          'Islamabad',
+          'Berlin',
+          'Washington',
+          'Paris',
+          'Rome',
+          'Tokyo',
+          'Budapest',
+          'Ottawa',
+          'Moscow',
+        ]}
+        value={value}
+        onChange={(ev) => {
+          setValue(ev.component.value);
+          console.log('ev.component', ev.component);
+        }}
+      />
+    </L.Div>
+  );
+};
