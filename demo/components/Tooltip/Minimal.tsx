373f672308291e30e3bec9d518300bbe5fe32ca6
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-11-27 12:05:04 +0300

    FEND-1153 [Tooltip] Added minimal demo example

---
+++ demo/components/Tooltip/Minimal.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as L from '../../../leda';
+import { StoryProps } from '../../types';
+
+export const Minimal = (storyProps: StoryProps) => {
+  return (
+    <L.Div>
+      <br />
+      <L.Tooltip
+        title="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat."
+      >
+        <L.Button _tipTop>Show tooltip</L.Button>
+      </L.Tooltip>
+    </L.Div>
+  );
+};
