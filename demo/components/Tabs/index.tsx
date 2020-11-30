a9b4a1533699b5eeb4ee77ba2e7a3190bda0e19e
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-11-30 11:21:00 +0300

    FEND-1153 [Tabs] Added minimal demo example (#180)

--- demo/components/Tabs/index.tsx
+++ demo/components/Tabs/index.tsx
@@ -3,11 +3,14 @@ import { Story } from '../Story';
 import { Tabs as TabsMain } from './Tabs';
 import { TabsNode } from './TabsNode';
 import { Scroll } from './Scroll';
+import { Minimal } from './Minimal';
+
 
 export const Tabs = () => (
   <Story title="Tabs">
     <TabsMain title="Tabs" />
     <TabsNode title="Tabs Node" />
     <Scroll title="Tabs scroll" />
+    <Minimal title="Minimal example" />
   </Story>
 );
