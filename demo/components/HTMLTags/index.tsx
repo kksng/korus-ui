6d0eb28a7db9888499de197a9f1f9158eef7a901
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-12-01 11:56:33 +0300

    FEND-1153 [HTML Tags] Added minimal demo example

--- demo/components/HTMLTags/index.tsx
+++ demo/components/HTMLTags/index.tsx
@@ -5,13 +5,15 @@ import { StringElements } from './StringElements';
 import { LayoutElements } from './LayoutElements';
 import { ListElements } from './ListElements';
 import { Images } from './Images';
+import { Minimal } from './Minimal';
 
 export const HTMLTags = () => (
-  <Story title="Верстка с помощью Leda">
-    <BlockElements title="Блочные элементы" />
-    <StringElements title="Строчные элементы" />
-    <LayoutElements title="Элементы размещения" />
-    <ListElements title="Списки" />
-    <Images title="Картинки" />
+  <Story title="Layout with Leda">
+    <BlockElements title="Block elements" />
+    <StringElements title="String elements" />
+    <LayoutElements title="Placement elements" />
+    <ListElements title="List" />
+    <Images title="Images" />
+    <Minimal title="Minimal example" />
   </Story>
 );
