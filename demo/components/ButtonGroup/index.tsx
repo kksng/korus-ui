47ede7470553e71c67c4f5553204a1ebd30d54a5
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-12-01 13:21:29 +0300

    FEND-1153 [ButtonGroup] Added minimal demo example

--- demo/components/ButtonGroup/index.tsx
+++ demo/components/ButtonGroup/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { Story } from '../Story';
 import { DataTypes } from './DataTypes';
 import { WorkTypes } from './WorkTypes';
+import { Minimal } from './Minimal';
 
 export const ButtonGroup = () => (
   <Story title="ButtonGroup">
-    <DataTypes title="Разные типы data" />
-    <WorkTypes title="Разные режимы работы" />
+    <DataTypes title="Different types data" />
+    <WorkTypes title="Different operating works" />
+    <Minimal title="Minimal example" />
   </Story>
 );
