f1ebb48620f9dc575af596d770372cf0ee7aac8f
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2020-12-02 12:50:20 +0300

    Added more examples and more tests

--- cypress/integration/Currency/index.spec.js
+++ cypress/integration/Currency/index.spec.js
@@ -16,4 +16,25 @@ describe('Currency', () => {
       cy.get('.eur').find('span').should('contain', '€');
     });
   });
+
+  describe('Customization', () => {
+    it('Сustom rendering of the currency sign (млн)', () => {
+      cy.get('.custom-rub')
+        .find('span')
+        .should('contain', 'млн')
+        .invoke('attr', 'class')
+        .should('contain', 'txt-danger');
+    });
+    it('If there is no value (placeholder)', () => {
+      cy.get('.placeholder-eur')
+        .find('span')
+        .should('contain', 'тут ничего нет');
+    });
+    it('Should trim fraction (false)', () => {
+      cy.get('.trim-eur').find('span').should('contain', '.00');
+    });
+    it('Should display 3 decimal places', () => {
+      cy.get('.prec').find('span').should('contain', '.000');
+    });
+  });
 });
--- demo/cypress/Currency/index.tsx
+++ demo/cypress/Currency/index.tsx
@@ -14,24 +14,88 @@ export const BasicUsage = (storyProps: StoryProps) => {
 
   return (
     <L.Div _box _inner _demoBg>
-      <L.Div _rub>
-        <L.Currency precision={2} currencyCode="RUB" {...props}>
-          12 550
-        </L.Currency>
+      <L.Div _basic>
+        <L.Div _rub>
+          <L.Currency precision={2} currencyCode="RUB" {...props}>
+            12 550
+          </L.Currency>
+        </L.Div>
+        <br />
+        <br />
+        <L.Div _usd>
+          <L.Currency precision={2} currencyCode="USD" {...props}>
+            12 550
+          </L.Currency>
+        </L.Div>
+        <br />
+        <br />
+        <L.Div _eur>
+          <L.Currency precision={2} currencyCode="EUR" {...props}>
+            12 550
+          </L.Currency>
+        </L.Div>
       </L.Div>
       <br />
+      <hr />
+      <L.Div>Customization</L.Div>
+      <hr />
       <br />
-      <L.Div _usd>
-        <L.Currency precision={2} currencyCode="USD" {...props}>
-          12 550
-        </L.Currency>
-      </L.Div>
-      <br />
-      <br />
-      <L.Div _eur>
-        <L.Currency precision={2} currencyCode="EUR" {...props}>
-          12 550
-        </L.Currency>
+      <L.Div _custom>
+        <L.Div _custom_rub>
+          <L.Currency
+            currencySymbolRender={({
+              elementProps: { children, style },
+            }: any) => (
+              <L.Span style={style} _txtDanger>
+                млн. {children}
+              </L.Span>
+            )}
+            wrapperRender={({ elementProps }: any) => <L.A {...elementProps} />}
+            precision={2}
+            currencyCode="RUB"
+            {...props}
+          >
+            12 550
+          </L.Currency>
+        </L.Div>
+        <br />
+        <hr />
+        <L.Div _placeholder>Placeholder</L.Div>
+        <hr />
+        <br />
+        <L.Div _placeholder_eur>
+          <L.Currency
+            precision={2}
+            currencyCode="EUR"
+            placeholder="тут ничего нет"
+            {...props}
+          ></L.Currency>
+        </L.Div>
+        <br />
+        <hr />
+        <L.Div>shouldTrimFraction=false</L.Div>
+        <hr />
+        <br />
+        <L.Div _trim_eur>
+          <L.Currency
+            precision={2}
+            currencyCode="EUR"
+            shouldTrimFraction={false}
+            {...props}
+          >
+            12 500.00
+          </L.Currency>
+        </L.Div>
+        <br />
+        <hr />
+        <L.Div>Precision=3</L.Div>
+        <hr />
+        <br />
+        <L.Div _prec>
+          <L.Currency precision={3} currencyCode="EUR" {...props}>
+            12 500.00
+          </L.Currency>
+        </L.Div>
       </L.Div>
     </L.Div>
   );
