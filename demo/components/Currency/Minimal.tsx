179ff58c8cfcab4cb0b9982bc6e743ad7148e6e0
Ellina <68591039+EllinaRakhmanova@users.noreply.github.com>
2020-12-03 13:37:32 +0300

    FEND-1153 [Currency] Removed optional props

--- demo/components/Currency/Minimal.tsx
+++ demo/components/Currency/Minimal.tsx
@@ -8,24 +8,6 @@ export const Minimal = (storyProps: StoryProps) => {
       <L.Currency>
         1000
       </L.Currency>
-      <br />
-      <L.Currency
-        currencyCode="RUB"
-      >
-        1000
-      </L.Currency>
-      <br />
-      <L.Currency
-        currencyCode="EUR"
-      >
-        1000
-      </L.Currency>
-      <br />
-      <L.Currency
-        currencyCode="USD"
-      >
-        1000
-      </L.Currency>
     </L.Div>
   ); 
 };
