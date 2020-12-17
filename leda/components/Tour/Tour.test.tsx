492da42aee3d6ddba85e5d0aee263e178d4a3100
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2020-12-17 14:21:16 +0300

    Fixed tests

--- leda/components/Tour/Tour.test.tsx
+++ leda/components/Tour/Tour.test.tsx
@@ -10,43 +10,45 @@ const rect = {
 
 const isScrolling = false;
 
+const padding = 10;
+
 const expected = {
   bottom: {
     left: '150px',
-    top: '220px',
+    top: '230px',
     transform: 'translateX(-50%)',
   },
   bottomLeft: {
     left: '200px',
-    top: '220px',
+    top: '230px',
     transform: 'translateX(-100%)',
   },
   bottomRight: {
     left: '100px',
-    top: '220px',
+    top: '230px',
   },
   left: {
-    left: '80px',
+    left: '70px',
     top: '100px',
     transform: 'translateX(-100%)',
   },
   right: {
-    left: '220px',
+    left: '230px',
     top: '100px',
   },
   top: {
     left: '150px',
-    top: '80px',
+    top: '70px',
     transform: 'translate(-50%, -100%)',
   },
   topLeft: {
     left: '200px',
-    top: '80px',
+    top: '70px',
     transform: 'translate(-100%, -100%)',
   },
   topRight: {
     left: '100px',
-    top: '80px',
+    top: '70px',
     transform: 'translateY(-100%)',
   },
 };
@@ -54,28 +56,44 @@ const expected = {
 describe('Tour getModalPositionStyles', () => {
   describe('Should calculate styles properly for existing positions', () => {
     it('Top position', () => {
-      expect(helpers.getModalPositionStyles(Position.Top, rect, isScrolling)).toEqual(expected.top);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.Top, rect,
+      })).toEqual(expected.top);
     });
     it('Bottom position', () => {
-      expect(helpers.getModalPositionStyles(Position.Bottom, rect, isScrolling)).toEqual(expected.bottom);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.Bottom, rect,
+      })).toEqual(expected.bottom);
     });
     it('Left position', () => {
-      expect(helpers.getModalPositionStyles(Position.Left, rect, isScrolling)).toEqual(expected.left);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.Left, rect,
+      })).toEqual(expected.left);
     });
     it('Right position', () => {
-      expect(helpers.getModalPositionStyles(Position.Right, rect, isScrolling)).toEqual(expected.right);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.Right, rect,
+      })).toEqual(expected.right);
     });
     it('TopLeft position', () => {
-      expect(helpers.getModalPositionStyles(Position.TopLeft, rect, isScrolling)).toEqual(expected.topLeft);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.TopLeft, rect,
+      })).toEqual(expected.topLeft);
     });
     it('TopRight position', () => {
-      expect(helpers.getModalPositionStyles(Position.TopRight, rect, isScrolling)).toEqual(expected.topRight);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.TopRight, rect,
+      })).toEqual(expected.topRight);
     });
     it('BottomLeft position', () => {
-      expect(helpers.getModalPositionStyles(Position.BottomLeft, rect, isScrolling)).toEqual(expected.bottomLeft);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.BottomLeft, rect,
+      })).toEqual(expected.bottomLeft);
     });
     it('BottomRight position', () => {
-      expect(helpers.getModalPositionStyles(Position.BottomRight, rect, isScrolling)).toEqual(expected.bottomRight);
+      expect(helpers.getModalPositionStyles({
+        isScrolling, padding, position: Position.BottomRight, rect,
+      })).toEqual(expected.bottomRight);
     });
   });
 });
