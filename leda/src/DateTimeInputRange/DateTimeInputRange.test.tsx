95886a3d4fec0e15e822a0096226ee4559304881
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2020-12-22 16:52:06 +0300

    FEND-1272 [DateRange] Fixed time for date values

---
+++ leda/src/DateTimeInputRange/DateTimeInputRange.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as helpers from './helpers';
+
+
+describe('Test normalizeTime helper function', () => {
+  it('Should work properly with date values', () => {
+    const dateValues = [new Date('01.02.2020'), new Date('01.02.2020')];
+    const expected = 'Thu Jan 02 2020 00:00:00 GMT+0300 (Moscow Standard Time),Thu Jan 02 2020 23:59:59 GMT+0300 (Moscow Standard Time)';
+
+    expect(dateValues.map(helpers.normalizeTime).join()).toEqual(expected);
+  });
+  it('Should work properly with null values', () => {
+    const dateValues = [null, null];
+    const expected = [null, null];
+
+    expect(dateValues.map(helpers.normalizeTime)).toEqual(expected);
+  });
+});
