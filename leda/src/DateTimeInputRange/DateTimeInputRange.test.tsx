fcdb39fd3fc6f312253c83ef6ae7d747d9c57b0c
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2020-12-23 10:12:11 +0300

    Fixed tests

--- leda/src/DateTimeInputRange/DateTimeInputRange.test.tsx
+++ leda/src/DateTimeInputRange/DateTimeInputRange.test.tsx
@@ -4,9 +4,9 @@ import * as helpers from './helpers';
 describe('Test normalizeTime helper function', () => {
   it('Should work properly with date values', () => {
     const dateValues = [new Date('01.02.2020'), new Date('01.02.2020')];
-    const expected = 'Thu Jan 02 2020 00:00:00 GMT+0300 (Moscow Standard Time),Thu Jan 02 2020 23:59:59 GMT+0300 (Moscow Standard Time)';
 
-    expect(dateValues.map(helpers.normalizeTime).join()).toEqual(expected);
+    expect(dateValues.map(helpers.normalizeTime)[0]?.toString()).toContain('00:00:00');
+    expect(dateValues.map(helpers.normalizeTime)[1]?.toString()).toContain('23:59:59');
   });
   it('Should work properly with null values', () => {
     const dateValues = [null, null];
