8798223c707426fd6ce36935891d3a694d3ce968
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2020-12-25 12:07:13 +0300

    FEND-1242 [TimeRange] Component tests

--- cypress/integration/TimeRange/index.spec.js
+++ cypress/integration/TimeRange/index.spec.js
@@ -1,13 +1,91 @@
 describe('TimeRange tests', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:9000/cypress/timerange');
+  beforeEach(() => {
+    cy.visit('http://localhost:9000/cypress/timerange');
+  });
+  describe('Interaction', () => {
+    it('Should move cursor to next input on Tab click', () => {
+      cy.name('TimeRange-from').tab().name('TimeRange-to').should('be.focused');
     });
-    describe('Interaction', () => {
-      it('Should move cursor to next input on Tab click', () => {
-        cy.name('TimeRange-from')
-          .tab()
-          .name('TimeRange-to')
-          .should('be.focused')
-      });
+    it('Basic use and entry of limit values', () => {
+      cy.get('.active')
+        .find('input')
+        .each((basicUse) => {
+          cy.wrap(basicUse)
+            .clear()
+            .type('12:30{enter}')
+            .should('have.value', '12:30')
+            .clear()
+            .paste('12:30{enter}')
+            .should('have.value', '12:30')
+            .clear()
+            .type('12:31{enter}')
+            .should('have.value', '12:31')
+            .clear()
+            .type('14:08{enter}')
+            .should('have.value', '14:08')
+            .clear()
+            .paste('14:08{enter}')
+            .should('have.value', '14:08')
+            .clear()
+            .type('16:59{enter}')
+            .should('have.value', '16:59')
+            .clear()
+            .type('17:00{enter}')
+            .should('have.value', '17:00')
+            .clear()
+            .paste('17:00{enter}')
+            .should('have.value', '17:00')
+            .clear();
+        });
+    });
+    it('Entering invalid values', () => {
+      cy.get('.active')
+        .find('input')
+        .each((enterInvalidValues) => {
+          cy.wrap(enterInvalidValues)
+            .clear()
+            .type('Hello')
+            .should('have.value', '__:__')
+            .type('Привет')
+            .should('have.value', '__:__')
+            .type('!@~`ёЁ')
+            .should('have.value', '__:__')
+            .type('    ')
+            .should('have.value', '__:__')
+            .clear();
+        });
+    });
+    it('Entering values out of range', () => {
+      cy.get('.active')
+        .find('input')
+        .each((enterOutOfRangeValues) => {
+          cy.wrap(enterOutOfRangeValues)
+            .clear()
+            .type('00:00{enter}')
+            .should('have.value', '12:30')
+            .clear()
+            .type('12:29{enter}')
+            .should('have.value', '12:30')
+            .clear()
+            .type('23:00{enter}')
+            .should('have.value', '17:00')
+            .clear()
+            .type('17:01{enter}')
+            .should('have.value', '17:00')
+            .clear()
+            .paste('17:01{enter}')
+            .should('have.value', '17:00')
+            .clear();
+        });
+    });
+    it('Should be disabled', () => {
+      cy.get('.disabled-state')
+        .find('input')
+        .each((shouldBeDisabled) => {
+          cy.wrap(shouldBeDisabled)
+            .should('be.disabled')
+            .and('have.attr', 'disabled');
+        });
     });
   });
+});
