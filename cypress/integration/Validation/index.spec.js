d67bf36380d85b5af10a8f935afcdf1326e7fba5
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-01-29 16:31:47 +0300

    Added an example. Working on tests.

--- cypress/integration/Validation/index.spec.js
+++ cypress/integration/Validation/index.spec.js
@@ -1,52 +1,234 @@
-import { globalDefaultTheme } from '../../../korus-ui/components/LedaProvider';
+// import { globalDefaultTheme } from '../../../korus-ui/components/LedaProvider';
 
-const theme = globalDefaultTheme.input;
+// const theme = globalDefaultTheme.input;
 
-const wrapperInvalid = theme.inputWrapperInvalid;
+// const wrapperInvalid = theme.inputWrapperInvalid;
+
+// describe('Validation', () => {
+//   beforeEach(() => {
+//     cy.visit('http://localhost:9000/cypress/validation');
+//   });
+
+//   describe('Display', () => {
+//     it('should add class danger if input is invalid', () => {
+//       cy.name('Toggle')
+//         .click()
+//         .name('Input1')
+//         .parent()
+//         .should(($element) => {
+//           expect($element).to.have.length(1);
+
+//           const className = $element[0].className;
+
+//           expect(className).to.contain(wrapperInvalid);
+//         })
+//         .name('Toggle')
+//         .click();
+//     });
+//     it('should display invalidMessage', () => {
+//       cy.name('Toggle')
+//         .click()
+//         .get('.invalid-message-item')
+//         .should('exist')
+//         .should(
+//           'have.text',
+//           'The app decides component to have invalid content'
+//         )
+//         .name('Toggle')
+//         .click();
+//     });
+//   });
+
+//   describe('Interaction', () => {
+//     it('submit should fail if input field is set as invalid', () => {
+//       cy.name('Toggle')
+//         .click()
+//         .name('Submit')
+//         .click()
+//         .name('Message')
+//         .should('have.text', 'Submit failed');
+//     });
+//   });
+// });
 
 describe('Validation', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:9000/cypress/validation');
-  });
-
-  describe('Display', () => {
-    it('should add class danger if input is invalid', () => {
-      cy.name('Toggle')
-        .click()
-        .name('Input1')
-        .parent()
-        .should(($element) => {
-          expect($element).to.have.length(1);
-
-          const className = $element[0].className;
-
-          expect(className).to.contain(wrapperInvalid);
-        })
-        .name('Toggle')
-        .click();
-    });
-    it('should display invalidMessage', () => {
-      cy.name('Toggle')
-        .click()
-        .get('.invalid-message-item')
-        .should('exist')
-        .should(
-          'have.text',
-          'The app decides component to have invalid content'
-        )
-        .name('Toggle')
-        .click();
-    });
-  });
+    cy.visit('http://localhost:9000/cypress/validation'),
+      {
+        onBeforeLoad(win) {
+          cy.stub(win.console, 'log').as('consoleLog')
+        },
+      }
+  })
 
   describe('Interaction', () => {
-    it('submit should fail if input field is set as invalid', () => {
-      cy.name('Toggle')
+    it('should be validate onBlur', () => {
+      cy.get('#ValidationRequiredBlur')
+        .find('input')
+        .each((validationOnBlur) => {
+          cy.wrap(validationOnBlur)
+            .focus()
+            .blur()
+            .parent()
+            .should('have.class', 'danger')
+            .and('have.class', 'required')
+            .parent()
+            .find('.invalid-message-item')
+            .should('contain', 'required')
+        })
+        .get('textarea')
+        .focus()
+        .blur()
+        .should('have.class', 'danger')
+        .and('have.class', 'required')
+        .parent()
+        .find('.invalid-message-item')
+        .should('contain', 'required')
+    })
+
+    it('should be validate onClick', () => {
+      cy.get('.basic')
+        .find('button')
         .click()
-        .name('Submit')
-        .click()
-        .name('Message')
-        .should('have.text', 'Submit failed');
-    });
-  });
-});
+        .parent()
+        .find('input')
+        .each((validationOnClick) => {
+          cy.wrap(validationOnClick)
+            .parent()
+            .should('have.class', 'danger')
+            .and('have.class', 'required')
+            .parent()
+            .find('.invalid-message-item')
+            .should('contain', 'required')
+        })
+        .get('textarea')
+        .should('have.class', 'danger')
+        .and('have.class', 'required')
+        .parent()
+        .find('.invalid-message-item')
+        .should('contain', 'required')
+    })
+    describe('using of types of validators', () => {
+      it('e-mail validator', () => {
+        cy.get('#propsValidator')
+          .name('email')
+          .type('1')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('have.text', 'Введите e-mail')
+          .name('email')
+          .clear()
+          .type('test@test')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('have.text', 'Введите e-mail')
+          .name('email')
+          .clear()
+          .type('test@test.test')
+          .blur()
+          .parent()
+          .should('not.have.class', 'danger')
+          .name('email')
+          .clear()
+      })
+      it('RegExp validator', () => {
+        cy.get('#propsValidator')
+          .name('regexp')
+          .type('11')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('contain', 'заглавная латинская')
+          .name('regexp')
+          .clear()
+          .type('aa11')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('contain', 'заглавная латинская')
+          .name('regexp')
+          .clear()
+          .type('Aa11')
+          .blur()
+          .parent()
+          .should('not.have.class', 'danger')
+          .name('regexp')
+          .clear()
+      })
+      it('function validator', () => {
+        cy.get('#propsValidator')
+          .name('function')
+          .type('1')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('contain', 'Минимум 10')
+          .name('function')
+          .clear()
+          .type('a1a1')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('contain', 'Минимум 10')
+          .name('function')
+          .clear()
+          .type('1q2/?#.$9-1')
+          .blur()
+          .parent()
+          .should('not.have.class', 'danger')
+      })
+      it('array of validators', () => {
+        cy.get('#propsValidator')
+          .name('array-field')
+          .focus()
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('have.text', 'Обязательное поле!')
+          .name('array-field')
+          .type('1')
+          .blur()
+          .parent()
+          .should('have.class', 'danger')
+          .parent()
+          .find('.invalid-message-list')
+          .should('contain', 'Введите e-mail')
+          .and('contain', 'Должна быть хотя бы одна заглавная буква!')
+          .and('contain', 'Минимум 10 символов!')
+      })
+      it.only('submit onClick', () => {
+        cy.get('#propsValidator')
+          .name('submitButton')
+          .click()
+          .parents('#propsValidator')
+          .find('input')
+          .each((empyFieldValidation) => {
+            cy.wrap(empyFieldValidation)
+              .parent()
+              .should('have.class', 'danger')
+              .parent()
+              .find('.invalid-message-list')
+              .should('contain', 'Обязательное')
+          })
+          .get('@consoleLog')
+          .should('be.calledWith', 'Click failed! Invalid fields')
+      })
+    })
+  })
+})
--- demo/cypress/Validation/index.tsx
+++ demo/cypress/Validation/index.tsx
@@ -1,37 +1,271 @@
-import * as React from 'react';
-import * as L from '../../../korus-ui';
+import * as React from 'react'
+import * as L from '../../../korus-ui'
+
+const Label = ({ children }: { children: string }) => (
+  <L.Span _width15 style={{ display: 'inline-block', marginRight: '10px' }}>
+    {children}
+  </L.Span>
+)
 
 export const Validation = () => {
-  const [isValid, setIsValid] = React.useState(true);
-  const [message, setMessage] = React.useState('');
+  // const [isValid, setIsValid] = React.useState(true);
+  // const [message, setMessage] = React.useState('');
 
+  const [ACValue, setACValue] = React.useState('')
+  const [DDSValue, setDDSValue] = React.useState<string | null>(null)
+
+  // return (
+  //   <L.Div _box _inner>
+  //     <L.Div>
+  //       <L.Div _inner>
+  //         <L.Input
+  //           isValid={isValid}
+  //           invalidMessage="The app decides component to have invalid content"
+  //           form="formIsValid"
+  //           name="Input1"
+  //           placeholder="outer isValid"
+  //         />
+  //       </L.Div>
+  //       <L.Div _inner>
+  //         <L.Button name="Toggle" onClick={() => setIsValid(!isValid)} _warning>
+  //           Toggle isValid
+  //         </L.Button>
+  //         <L.Button
+  //           name="Submit"
+  //           form="formIsValid"
+  //           onClick={() => setMessage('Submitted')}
+  //           onValidationFail={() => setMessage('Submit failed')}
+  //         >
+  //           Submit
+  //         </L.Button>
+  //         <L.Div name="Message">{message}</L.Div>
+  //       </L.Div>
+  //     </L.Div>
+  //   </L.Div>
+  // );
   return (
     <L.Div _box _inner>
-      <L.Div>
-        <L.Div _inner>
-          <L.Input
-            isValid={isValid}
-            invalidMessage="The app decides component to have invalid content"
-            form="formIsValid"
-            name="Input1"
-            placeholder="outer isValid"
+      <L.Div _basic>
+        <L.Div id="ValidationRequiredBlur">
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>AutoComplete</Label>
+            <L.AutoComplete
+              data={['Berlin', 'Paris']}
+              onChange={(ev) => setACValue(ev.component.value)}
+              value={ACValue}
+              isRequired
+              form="requiredForm"
+              name="AutoComplete"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>DatePicker</Label>
+            <L.DatePicker
+              isRequired
+              form="requiredForm"
+              name="DatePicker"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>DateTimePicker</Label>
+            <L.DateTimePicker
+              isRequired
+              form="requiredForm"
+              name="DateTimePicker"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>DateRange</Label>
+            <L.DateRange
+              isRequired
+              form="requiredForm"
+              name="DateRange"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>DateTimeRange</Label>
+            <L.DateTimeRange
+              isRequired
+              form="requiredForm"
+              name="DateTimeRange"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>DropDownSelect</Label>
+            <L.DropDownSelect
+              value={DDSValue}
+              onChange={(ev) => setDDSValue(ev.component.value)}
+              isRequired
+              form="requiredForm"
+              name="DropDownSelect"
+              placeholder="Berlin or Paris?"
+              data={['Berlin', 'Paris']}
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>Input</Label>
+            <L.Input
+              isRequired
+              form="requiredForm"
+              name="Input"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+            <br />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>MaskedInput</Label>
+            <L.MaskedInput
+              isRequired
+              form="requiredForm"
+              name="MaskedInput"
+              mask="###-###"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>MultiSelect</Label>
+            <L.MultiSelect
+              isRequired
+              form="requiredForm"
+              name="MultiSelect"
+              data={['Berlin', 'Paris']}
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          {/* <L.Div _inner _flexRow _alignItemsCenter>
+          <Label>
+            NumericRange
+          </Label>
+          <L.NumericRange
+            isRequired
+            form="requiredForm"
+            name="NumericRange"
+            requiredMessage={'Field is required'}
+            _grow1
           />
-        </L.Div>
-        <L.Div _inner>
-          <L.Button name="Toggle" onClick={() => setIsValid(!isValid)} _warning>
-            Toggle isValid
-          </L.Button>
+        </L.Div> */}
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>NumericTextBox</Label>
+            <L.NumericTextBox
+              isRequired
+              form="requiredForm"
+              name="NumericTextBox"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
+          <L.Div _inner _flexRow _alignItemsCenter>
+            <Label>Textarea</Label>
+            <L.Textarea
+              isRequired
+              form="requiredForm"
+              name="Textarea"
+              requiredMessage={'Field is required'}
+              _grow1
+            />
+          </L.Div>
           <L.Button
-            name="Submit"
-            form="formIsValid"
-            onClick={() => setMessage('Submitted')}
-            onValidationFail={() => setMessage('Submit failed')}
+            form={['requiredForm']}
+            scrollOffset={64}
+            shouldScrollToInvalidFields
+            _warning
+            onClick={() => console.log('Successful click!')}
+            onValidationFail={(ev) =>
+              console.log('Click failed! Invalid fields', ev.invalidForms)
+            }
           >
-            Submit
+            Валидировать!
           </L.Button>
-          <L.Div name="Message">{message}</L.Div>
+        </L.Div>
+      </L.Div>
+      <br />
+      <br />
+      <L.Div _box id="propsValidator">
+        <L.Div>
+          <L.Div _inner>
+            <L.Input
+              form="form1"
+              name="email"
+              validator="email"
+              isRequired
+              requiredMessage="Обязательное поле!"
+              placeholder="predefined email validator"
+            />
+          </L.Div>
+          <L.Div _inner>
+            <L.Input
+              form="form1"
+              name="regexp"
+              validator={/[A-Z]/}
+              isRequired
+              requiredMessage="Обязательное поле!"
+              invalidMessage="Должна быть хотя бы одна заглавная латинская буква!"
+              placeholder="validator as RegExp"
+            />
+          </L.Div>
+          <L.Div _inner>
+            <L.Input
+              form="form1"
+              name="function"
+              validator={(value: string): boolean =>
+                value.toString().length > 10
+              }
+              invalidMessage="Минимум 10 символов!"
+              isRequired
+              requiredMessage="Обязательное поле!"
+              placeholder="validator as function"
+            />
+          </L.Div>
+          <L.Div _inner>
+            <L.Input
+              form="form1"
+              name="array-field"
+              validator={[
+                { validator: 'email' },
+                {
+                  validator: /[A-Z]/,
+                  invalidMessage: 'Должна быть хотя бы одна заглавная буква!',
+                },
+                {
+                  validator: (value: string) => value.toString().length > 10,
+                  invalidMessage: 'Минимум 10 символов!',
+                },
+              ]}
+              isRequired
+              requiredMessage="Обязательное поле!"
+              placeholder="validator as an array of validators"
+            />
+          </L.Div>
+          <L.Div _inner>
+            <L.Button
+              form="form1"
+              name="submitButton"
+              _warning
+              onClick={() => console.log('Successful click!')}
+              onValidationFail={(ev) =>
+                console.log('Click failed! Invalid fields', ev.invalidForms)
+              }
+            >
+              Submit
+            </L.Button>
+          </L.Div>
         </L.Div>
       </L.Div>
     </L.Div>
-  );
-};
+  )
+}
