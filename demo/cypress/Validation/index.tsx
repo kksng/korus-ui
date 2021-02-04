f4c47101b86c7d7af5f7eb67cb007a8ecf254c52
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-02-04 10:36:47 +0300

    Fixed style

--- demo/cypress/Validation/index.tsx
+++ demo/cypress/Validation/index.tsx
@@ -1,23 +1,20 @@
-import * as React from "react";
-import * as L from "../../../korus-ui";
-import { BasicForm } from "./BasicForm";
-import { ControlledValidation } from "./ControlledValidation";
-import { NumericRangeValidation } from "./NumericRangeValidation.tsx";
-import { PredefinedValidators } from "./PredefinedValidators";
-import { ScrollForm, SubmitScrollForm } from "./ScrollForm";
-import { ValidationTypes } from "./ValidationTypes";
+import * as React from 'react';
+import * as L from '../../../korus-ui';
+import { BasicForm } from './BasicForm';
+import { ControlledValidation } from './ControlledValidation';
+import { NumericRangeValidation } from './NumericRangeValidation.tsx';
+import { PredefinedValidators } from './PredefinedValidators';
+import { ScrollForm, SubmitScrollForm } from './ScrollForm';
+import { ValidationTypes } from './ValidationTypes';
 
-export const Validation = () => {
-
-  return (
-    <L.Div _box _inner>
-      <ScrollForm />
-      <ControlledValidation />
-      <BasicForm />
-      <ValidationTypes />
-      <PredefinedValidators />
-      <NumericRangeValidation />
-      <SubmitScrollForm />
-    </L.Div>
-  );
-};
+export const Validation = () => (
+  <L.Div _box _inner>
+    <ScrollForm />
+    <ControlledValidation />
+    <BasicForm />
+    <ValidationTypes />
+    <PredefinedValidators />
+    <NumericRangeValidation />
+    <SubmitScrollForm />
+  </L.Div>
+);
