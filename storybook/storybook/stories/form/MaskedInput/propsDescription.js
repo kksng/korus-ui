5bd37e9618ac30c233fff5150cbb3c86444233a9
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-02-04 16:14:21 +0300

    Updated Storybook

--- storybook/storybook/stories/form/MaskedInput/propsDescription.js
+++ storybook/storybook/stories/form/MaskedInput/propsDescription.js
@@ -239,12 +239,14 @@ interface MaskedInputRefCurrent {
       <L.Tooltip title={(
         <pre>{`
 {
-  inputWrapperInvalid:      string,
-  inputWrapperDisabled:     string,
-  inputWrapperFocused:      string,
-  inputWrapper:             string,
-  input:                    string,
-  wrapper:                  string,
+  closeIcon:                'masked-input-clear-icon',
+  input:                    'masked-input-element',
+  inputWrapper:             'masked-input-wrapper',
+  inputWrapperDisabled:     'disabled',
+  inputWrapperFocused:      'focused',
+  inputWrapperInvalid:      'danger',
+  inputWrapperRequired:     'required',
+  wrapper:                  'masked-wrapper',
 }
         `}
         </pre>
