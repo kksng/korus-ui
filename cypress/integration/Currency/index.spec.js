83a5aae6cb1063c9bb3d80e78dcc07531f88dfdc
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-02-17 17:26:16 +0300

    [Currency] Refactor tests

--- cypress/integration/Currency/index.spec.js
+++ cypress/integration/Currency/index.spec.js
@@ -5,36 +5,42 @@ describe('Currency', () => {
 
   describe('Display', () => {
     it('Should render rubles', () => {
-      cy.get('.rub').find('span').should('contain', '₽');
+      cy.name('rub')
+        .should('contain', '₽');
     });
 
     it('Should render dollars', () => {
-      cy.get('.usd').find('span').should('contain', '$');
+      cy.name('usd')
+        .should('contain', '$');
     });
 
     it('Should render euros', () => {
-      cy.get('.eur').find('span').should('contain', '€');
+      cy.name('eur')
+        .should('contain', '€');
     });
   });
 
   describe('Customization', () => {
     it('Сustom rendering of the currency sign (млн)', () => {
-      cy.get('.custom-rub')
+      cy.name('customRub')
         .find('span')
         .should('contain', 'млн')
-        .invoke('attr', 'class')
-        .should('contain', 'txt-danger');
+        .and('have.class', 'txt-danger');
     });
+
     it('If there is no value (placeholder)', () => {
-      cy.get('.placeholder-eur')
-        .find('span')
+      cy.name('placeholderEur')
         .should('contain', 'тут ничего нет');
     });
+
     it('Should trim fraction (false)', () => {
-      cy.get('.trim-eur').find('span').should('contain', '.00');
+      cy.name('trimEur')
+        .should('contain', '.00');
     });
+
     it('Should display 3 decimal places', () => {
-      cy.get('.prec').find('span').should('contain', '.000');
+      cy.name('prec')
+        .should('contain', '.000');
     });
   });
 });
