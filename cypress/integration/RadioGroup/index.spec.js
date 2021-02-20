6d8dda2f26977dda1970ecd31ddd4c00bdec5b42
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-02-20 17:20:46 +0300

    [RadioGroup] Refactor tests

--- cypress/integration/RadioGroup/index.spec.js
+++ cypress/integration/RadioGroup/index.spec.js
@@ -4,21 +4,23 @@ describe('RadioGroup', () => {
   });
 
   describe('Display', () => {
-    it('should render first radio checked in uncontrolled mode', () => {
+    it('Should render first radio checked in uncontrolled mode', () => {
       cy.get('.uncontrolled')
         .find('.radio-button')
         .first()
         .find('input')
         .should('be.checked')
     });
-    it('should render value checked if it is passed in controlled mode', () => {
+    
+    it('Should render value checked if it is passed in controlled mode', () => {
       cy.get('.controlled-with-initial-value')
         .find('.radio-button')
         .last()
         .find('input')
         .should('be.checked')
     });
-    it('should render first radio checked in controlled mode if no value provided', () => {
+
+    it('Should render first radio checked in controlled mode if no value provided', () => {
       cy.get('.controlled-without-initial-value')
         .find('.radio-button')
         .first()
@@ -28,7 +30,7 @@ describe('RadioGroup', () => {
   });
 
   describe('Interaction', () => {
-    it('should reset to first value in uncontrolled mode', () => {
+    it('Should reset to first value in uncontrolled mode', () => {
       cy.get('.uncontrolled')
         .find('.radio-button')
         .last()
@@ -43,7 +45,8 @@ describe('RadioGroup', () => {
         .find('input')
         .should('be.checked')
     });
-    it('should reset to initial value in controlled mode', () => {
+
+    it('Should reset to initial value in controlled mode', () => {
       cy.get('.controlled-with-initial-value')
         .find('.radio-button')
         .eq(1)
@@ -58,7 +61,8 @@ describe('RadioGroup', () => {
         .find('input')
         .should('be.checked')       
     });
-    it('should reset to first value in controlled mode without initial value', () => {
+    
+    it('Should reset to first value in controlled mode without initial value', () => {
       cy.get('.controlled-without-initial-value')
         .find('.radio-button')
         .eq(1)
