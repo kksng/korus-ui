672178664e4a1541253a72f5d5cae5c9d4768378
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-02-24 14:17:38 +0300

    [Button] Сorrections based on comments

--- demo/cypress/Button/index.tsx
+++ demo/cypress/Button/index.tsx
@@ -16,14 +16,13 @@ export const Button = (): React.ReactElement => (
     >
       Validate!
     </L.Button>
-    <L.Button onClick={() => alert('Alert!')}>Клик!</L.Button>
-    <L.Button isLoading onClick={() => alert('Alert!')}>isLoading</L.Button>
-    <L.Button isDisabled onClick={() => alert('Alert!')}>isDisabled</L.Button>
+    <L.Button name="basicButton" onClick={() => alert('Alert!')}>Click!</L.Button>
+    <L.Button name="loadButton" isLoading onClick={() => alert('Alert!')}>isLoading</L.Button>
+    <L.Button name="disabledButton" isDisabled>isDisabled</L.Button>
     <L.Button _danger>danger!</L.Button>
     <L.Button _warning>warning!</L.Button>
     <L.Button _success>success!</L.Button>
     <L.Button name="consoleButton" onClick={() => console.log('Do not click the button!')}>Don't click!</L.Button>
-    <L.Button _success>success!</L.Button>
     <div
       style={{
         height: '100vh',
