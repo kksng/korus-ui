a9cdf184d62bad30b6d1392f4bf8dae66d813dd9
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-02-26 19:14:06 +0300

    [Tooltip] Refactor tests

--- cypress/integration/Tooltip/index.spec.js
+++ cypress/integration/Tooltip/index.spec.js
@@ -3,21 +3,21 @@ describe('ToolTip tests', () => {
     cy.visit('http://localhost:9000/cypress/tooltip');
   });
   describe('Display', () => {
-    it('should choose right side correctly without positioning', () => {
+    it('Should choose right side correctly without positioning', () => {
       cy.name('rightSide')
         .click()
         .get('.tooltip')
         .eq(6)
         .should('have.class', 'right');
     });
-    it('should choose left side correctly without positioning', () => {
+    it('Should choose left side correctly without positioning', () => {
       cy.name('leftSide')
         .click()
         .get('.tooltip')
         .eq(7)
         .should('have.class', 'left');
     });
-    it('should choose top side by default, if enough space', () => {
+    it('Should choose top side by default, if enough space', () => {
       cy.name('topSidePressedToLeft')
         .click()
         .get('.tooltip')
@@ -29,7 +29,7 @@ describe('ToolTip tests', () => {
         .eq(9)
         .should('have.class', 'top');  
     });
-    it('should define corner position', () => {
+    it('Should define corner position', () => {
       cy.name('topLeftCorner')
         .click()
         .get('.tooltip')
