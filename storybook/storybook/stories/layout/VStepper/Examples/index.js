8bd28817626fbf4b05fc8dfaf00847950b14f86b
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-03 18:12:51 +0300

    FEND-1366 [VStepper] Description of the component docs in Storybook

--- storybook/storybook/stories/layout/VStepper/Examples/index.js
+++ storybook/storybook/stories/layout/VStepper/Examples/index.js
@@ -1,5 +1,7 @@
-import componentSrc from '!raw-loader!@korus/leda/dist/components/Tour/';
+import componentSrc from '!raw-loader!@korus/leda/dist/components/VStepper/';
 
 export { componentSrc };
 export { Api } from './Api';
+export { BasicUsage } from './BasicUsage';
 export { DynamicSteps } from './DynamicSteps';
+export { Customization } from './Customization';
--- storybook/storybook/stories/layout/VStepper/VStepper.stories.js
+++ storybook/storybook/stories/layout/VStepper/VStepper.stories.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Api, DynamicSteps } from './Examples';
+import { Api, BasicUsage, DynamicSteps, Customization } from './Examples';
 import { propsDesc } from './propsDescription';
 import { ApiStory } from '../../../components/ApiStory';
 
@@ -8,6 +8,12 @@ storiesOf('Layout|VStepper', module)
   .add('API', (context) => (
     <ApiStory {...Api} customProps={propsDesc} context={context} />
   ))
-  .add('DynamicSteps', (context) => (
+  .add('Базовый пример', (context) => (
+    <ApiStory {...BasicUsage} customProps={propsDesc} context={context} />
+  ))
+  .add('Динамические шаги', (context) => (
     <ApiStory {...DynamicSteps} customProps={propsDesc} context={context} />
+  ))
+  .add('Расширяемость', (context) => (
+    <ApiStory {...Customization} customProps={propsDesc} context={context} />
   ));
