083a4b5e0d5e7e0722ff625ef7fb6376312ec6c4
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-04 14:26:15 +0300

    Fixed formatting

--- storybook/storybook/stories/layout/VStepper/VStepper.stories.js
+++ storybook/storybook/stories/layout/VStepper/VStepper.stories.js
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Api, BasicUsage, DynamicSteps, Customization } from './Examples';
+import { 
+  Api, 
+  BasicUsage, 
+  DynamicSteps, 
+  Customization 
+} from './Examples';
 import { propsDesc } from './propsDescription';
+import { Story } from '../../../components/Story';
 import { ApiStory } from '../../../components/ApiStory';
 
 storiesOf('Layout|VStepper', module)
-  .add('API', (context) => (
-    <ApiStory {...Api} customProps={propsDesc} context={context} />
-  ))
-  .add('Базовый пример', (context) => (
-    <ApiStory {...BasicUsage} customProps={propsDesc} context={context} />
-  ))
-  .add('Динамические шаги', (context) => (
-    <ApiStory {...DynamicSteps} customProps={propsDesc} context={context} />
-  ))
-  .add('Расширяемость', (context) => (
-    <ApiStory {...Customization} customProps={propsDesc} context={context} />
-  ));
+  .add('API', (context) => (<ApiStory {...Api} customProps={propsDesc} context={context} />))
+  .add('Базовый пример', (context) => (<Story {...BasicUsage} customProps={propsDesc} context={context} />))
+  .add('Динамические шаги', (context) => (<Story {...DynamicSteps} customProps={propsDesc} context={context} />))
+  .add('Расширяемость', (context) => (<Story {...Customization} customProps={propsDesc} context={context} />));
