dbc3d21d8b5d246d75011f69275c8e8e4ab6a7ac
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-09 09:13:27 +0300

    [Button] Refactor tests (#345)
    * [Button] Refactor tests
    
    * [Button] Сorrections based on comments
    
    * Replacing name with id. Import a component theme
    
    Co-authored-by: MickRomanov <57683220+MickRomanov@users.noreply.github.com>

--- demo/cypress/Button/index.tsx
+++ demo/cypress/Button/index.tsx
@@ -10,20 +10,19 @@ export const Button = (): React.ReactElement => (
     <L.Button
       shouldScrollToInvalidFields
       form={['form1', 'form2']}
-      name='validation'
+      id="validation"
       onClick={() => alert('Submitted')}
       onValidationFail={() => alert('Alert!')}
     >
       Validate!
     </L.Button>
-    <L.Button onClick={() => alert('Alert!')}>Клик!</L.Button>
-    <L.Button isLoading onClick={() => alert('Alert!')}>isLoading</L.Button>
-    <L.Button isDisabled onClick={() => alert('Alert!')}>isDisabled</L.Button>
+    <L.Button id="basicButton" onClick={() => alert('Alert!')}>Click!</L.Button>
+    <L.Button id="loadButton" isLoading onClick={() => alert('Alert!')}>isLoading</L.Button>
+    <L.Button id="disabledButton" isDisabled>isDisabled</L.Button>
     <L.Button _danger>danger!</L.Button>
     <L.Button _warning>warning!</L.Button>
     <L.Button _success>success!</L.Button>
-    <L.Button name='consoleButton' onClick={() => console.log('Do not click the button!')}>Don't click!</L.Button>
-    <L.Button _success>success!</L.Button>
+    <L.Button id="consoleButton" onClick={() => console.log('Do not click the button!')}>Don't click!</L.Button>
     <div
       style={{
         height: '100vh',
