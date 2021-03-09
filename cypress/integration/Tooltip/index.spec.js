b212a04525eb6d333e805b1a110e1db54ae9aae4
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-09 09:18:34 +0300

    [Tooltip] Refactor tests (#355)
    * [Tooltip] Refactor tests
    
    * Replacing name with id
    
    Co-authored-by: MickRomanov <57683220+MickRomanov@users.noreply.github.com>

--- cypress/integration/Tooltip/index.spec.js
+++ cypress/integration/Tooltip/index.spec.js
@@ -3,39 +3,39 @@ describe('ToolTip tests', () => {
     cy.visit('/cypress/tooltip');
   });
   describe('Display', () => {
-    it('should choose right side correctly without positioning', () => {
-      cy.name('rightSide')
+    it('Should choose right side correctly without positioning', () => {
+      cy.get('#rightSide')
         .click()
         .get('.tooltip')
         .eq(6)
         .should('have.class', 'right');
     });
-    it('should choose left side correctly without positioning', () => {
-      cy.name('leftSide')
+    it('Should choose left side correctly without positioning', () => {
+      cy.get('#leftSide')
         .click()
         .get('.tooltip')
         .eq(7)
         .should('have.class', 'left');
     });
-    it('should choose top side by default, if enough space', () => {
-      cy.name('topSidePressedToLeft')
+    it('Should choose top side by default, if enough space', () => {
+      cy.get('#topSidePressedToLeft')
         .click()
         .get('.tooltip')
         .eq(8)
         .should('have.class', 'top');    
-      cy.name('topSidePressedToRight')
+      cy.get('#topSidePressedToRight')
         .click()
         .get('.tooltip')
         .eq(9)
         .should('have.class', 'top');  
     });
-    it('should define corner position', () => {
-      cy.name('topLeftCorner')
+    it('Should define corner position', () => {
+      cy.get('#topLeftCorner')
         .click()
         .get('.tooltip')
         .first()
         .should('have.class', 'bottom-right');    
-      cy.name('topRightCorner')
+      cy.get('#topRightCorner')
         .click()
         .get('.tooltip')
         .eq(1)
