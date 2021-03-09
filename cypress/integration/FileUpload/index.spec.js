681688c42a055f4103038b482b713909eda0bb67
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-09 09:17:08 +0300

    [FileUpload] Refactor tests (#357)
    * [FileUpload] Refactor tests
    
    * Replacing name with id
    
    Co-authored-by: MickRomanov <57683220+MickRomanov@users.noreply.github.com>

--- cypress/integration/FileUpload/index.spec.js
+++ cypress/integration/FileUpload/index.spec.js
@@ -12,9 +12,11 @@ describe('FileUpload', () => {
       const stub = cy.stub();
       cy.on('window:alert', stub);
       const fileName = 'test.png';
-      cy.get('.custom').contains('Загрузить').click();
+      cy.get('.custom')
+        .contains('Загрузить')
+        .click();
       cy.fixture('test.png').then((uploadFile) => {
-        cy.name('customFileUpload')
+        cy.get('#customFileUpload')
           .attachFile(fileName, uploadFile, 'image/png')
           .get('button')
           .should('have.class', 'loading')
@@ -29,9 +31,11 @@ describe('FileUpload', () => {
       const stub = cy.stub();
       cy.on('window:alert', stub);
       const fileName = 'test.png';
-      cy.get('.controlled').contains('Загрузить').click();
+      cy.get('.controlled')
+        .contains('Загрузить')
+        .click();
       cy.fixture('test.png').then((uploadFile) => {
-        cy.name('controlledFileUpload')
+        cy.get('#controlledFileUpload')
           .attachFile(fileName, uploadFile, 'image/png')
           .get('.controlled')
           .should('contain', 'Загрузка')
@@ -46,9 +50,11 @@ describe('FileUpload', () => {
       const stub = cy.stub();
       const invalidFile = 'txtFile.txt';
       cy.on('window:alert', stub);
-      cy.get('.partialcustom').contains('частично').click();
+      cy.get('.partialcustom')
+        .contains('частично')
+        .click();
       cy.fixture('txtFile.txt').then((uploadInvalidFile) => {
-        cy.name('partialCustomFileUpload')
+        cy.get('#partialCustomFileUpload')
           .attachFile(invalidFile, uploadInvalidFile, 'text/*')
           .get('.partialcustom')
           .should('contain', 'загружаю')
@@ -65,9 +71,11 @@ describe('FileUpload', () => {
       const stub = cy.stub();
       const bigFile = 'bigFile.jpeg';
       cy.on('window:alert', stub);
-      cy.get('.partialcustom').contains('частично').click();
+      cy.get('.partialcustom')
+        .contains('частично')
+        .click();
       cy.fixture('bigFile.jpeg').then((uploadBigFile) => {
-        cy.name('partialCustomFileUpload')
+        cy.get('#partialCustomFileUpload')
           .attachFile(bigFile, uploadBigFile, 'image/jpg')
           .get('.partialcustom')
           .should('contain', 'загружаю')
@@ -84,9 +92,11 @@ describe('FileUpload', () => {
       const stub = cy.stub();
       const smallFile = 'test.png';
       cy.on('window:alert', stub);
-      cy.get('.partialcustom').contains('частично').click();
+      cy.get('.partialcustom')
+        .contains('частично')
+        .click();
       cy.fixture('test.png').then((uploadSmallFile) => {
-        cy.name('partialCustomFileUpload')
+        cy.get('#partialCustomFileUpload')
           .attachFile(smallFile, uploadSmallFile, 'image/png')
           .get('.partialcustom')
           .should('contain', 'загружаю')
@@ -103,9 +113,11 @@ describe('FileUpload', () => {
       const stub = cy.stub();
       cy.on('window:alert', stub);
       const fileName = 'bigFile.jpeg';
-      cy.get('.controlled').contains('Загрузить').click();
+      cy.get('.controlled')
+        .contains('Загрузить')
+        .click();
       cy.fixture('bigFile.jpeg').then((uploadLongNameFile) => {
-        cy.name('controlledFileUpload')
+        cy.get('#controlledFileUpload')
           .attachFile(fileName, uploadLongNameFile, 'image/jpg')
           .get('.controlled')
           .should('contain', 'Загрузка')
