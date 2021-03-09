d447e8a8b7b29e67f8a30c18c6af70ee83662adf
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-09 09:17:57 +0300

    [MaskedInput] Refactor tests (#356)
    * [MaskedInput] Refactor tests
    
    * Replacing name with id
    
    Co-authored-by: MickRomanov <57683220+MickRomanov@users.noreply.github.com>

--- cypress/integration/MaskedInput/index.spec.js
+++ cypress/integration/MaskedInput/index.spec.js
@@ -12,7 +12,7 @@ describe('MaskedInput', () => {
 
   describe('Interaction', () => {
     describe('Input', () => {
-      it('should clear one char per backspace press', () => {
+      it('Should clear one char per backspace press', () => {
         cy.name('MINotControlledPhone')
           .focusMasked()
           .clear()
@@ -37,7 +37,7 @@ describe('MaskedInput', () => {
           .should('have.value', '+7 (___)-___-__-__');
       });
 
-      it('should move cursor if backspace pressed on empty mask', () => {
+      it('Should move cursor if backspace pressed on empty mask', () => {
         cy.name('MINotControlledPhone')
           .focusMasked()
           .type('{rightArrow}'.repeat(7))
@@ -56,7 +56,7 @@ describe('MaskedInput', () => {
           .should('have.value', '+7 (___)-___-__-__');
       });
 
-      it('should clear masked value on clear button click', () => {
+      it('Should clear masked value on clear button click', () => {
         cy.name('MINotControlledPhone')
           .type('9818862798')
           .should('have.value', '+7 (981)-886-27-98') 
@@ -73,8 +73,8 @@ describe('MaskedInput', () => {
           .should('have.value', '+7 (___)-___-__-__');
       });
 
-      it('should fill different masks', () => {
-        cy.name('MIControlledPhone')
+      it('Should fill different masks', () => {
+        cy.get('#MIControlledPhone')
           .should('have.value', '+7 (800)-200-06-00')
           .focusMasked()
           .clear()
@@ -82,20 +82,20 @@ describe('MaskedInput', () => {
           .type('9818862798')
           .should('have.value', '+7 (981)-886-27-98')
           .closest('.demo-story')
-          .name('MIControlledInsurance')
+          .get('#MIControlledInsurance')
           .should('have.attr', 'placeholder', '___-___-___ __')
           .focusMasked()
           .clear()
           .type('12345678901')
           .should('have.value', '123-456-789 01')
           .closest('.demo-story')
-          .name('MICarNumber')
+          .get('#MICarNumber')
           .should('have.attr', 'placeholder', 'Car number')
           .focusMasked()
           .type('AA12BB3456')
           .should('have.value', 'AA12BB3456')
           .closest('.demo-story')
-          .name('MICreditCardNumber')
+          .get('#MICreditCardNumber')
           .should('have.value', '6666-7777-8888-9999')
           .focusMasked()
           .clear()
@@ -103,7 +103,7 @@ describe('MaskedInput', () => {
           .should('have.value', '1234-1234-1234-1234');
       });
 
-      it('should forbid non-mask chars', () => {
+      it('Should forbid non-mask chars', () => {
         cy.name('MINotControlledPhone')
           .focusMasked()
           .should('have.value', '+7 (___)-___-__-__')
@@ -113,7 +113,7 @@ describe('MaskedInput', () => {
     });
 
     describe('Validation', () => {
-      it('should be invalid when isRequired and value is empty', () => {
+      it('Should be invalid when isRequired and value is empty', () => {
         cy.name('MINotControlledPhone')
           .focusMasked()
           .blur()
@@ -123,7 +123,7 @@ describe('MaskedInput', () => {
           .should('contain', 'Обязательное поле!');
       });
 
-      it('should be invalid when isRequired and value is not complete', () => {
+      it('Should be invalid when isRequired and value is not complete', () => {
         cy.name('MINotControlledPhone')
           .focusMasked()
           .type('1234')
@@ -134,7 +134,7 @@ describe('MaskedInput', () => {
           .should('contain', 'Обязательное поле!');
       });
 
-      it('should be valid when isRequired and value is complete', () => {
+      it('Should be valid when isRequired and value is complete', () => {
         cy.name('MINotControlledPhone')
           .focusMasked()
           .type('9818862798')
@@ -149,48 +149,48 @@ describe('MaskedInput', () => {
 
   describe('Rest', () => {
     describe('Controlled mode', () => {
-      it('should clear and set value', () => {
-        cy.name('MIControlledPhone')
+      it('Should clear and set value', () => {
+        cy.get('#MIControlledPhone')
           .clear()
           .type('9818862798')
           .should('have.value', '+7 (981)-886-27-98')
-          .name('clearPhoneValue')
+          .get('#clearPhoneValue')
           .click()
-          .name('MIControlledPhone')
+          .get('#MIControlledPhone')
           .should('not.have.value')
-          .name('setPhoneValue')
+          .get('#setPhoneValue')
           .click()
-          .name('MIControlledPhone')
+          .get('#MIControlledPhone')
           .should('have.value', '+7 (981)-886-27-98');
       });
-      it('should clear mask value', () => {
-        cy.name('clearPhoneValue')
+      it('Should clear mask value', () => {
+        cy.get('#clearPhoneValue')
           .click()
-          .name('MIControlledPhone')
+          .get('#MIControlledPhone')
           .type('{enter}')
           .get('@consoleLog')
           .should('be.calledWith', '+7 (___)-___-__-__')
-          .name('MIControlledPhone')
+          .get('#MIControlledPhone')
           .type('8888')
           .get('@consoleLog')
           .should('be.calledWith', '+7 (888)-8__-__-__')
-          .name('clearPhoneValue')
+          .get('#clearPhoneValue')
           .click()
-          .name('MIControlledPhone')
+          .get('#MIControlledPhone')
           .type('{enter}')
           .get('@consoleLog')
           .should('be.calledWith', '+7 (___)-___-__-__')
       });
-      it('should remove mask when value is cleared', () => {
-        cy.name('MICardNumberControlled')
+      it('Should remove mask when value is cleared', () => {
+        cy.get('#MICardNumberControlled')
           .type('33')
           .get('@consoleLog')
           .should('be.calledWith', '33__-____-____-____')
-          .name('clearCardValue')
+          .get('#clearCardValue')
           .click()
           .get('@consoleLog')
           .should('be.calledWith', '')
-          .name('MICardNumberControlled')
+          .get('#MICardNumberControlled')
       });
     });
   });
