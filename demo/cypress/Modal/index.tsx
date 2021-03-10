942b43c5949f3e2cff2a75394d91b1d8f3aaa742
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-10 09:01:49 +0300

    [Modal] Refactor tests (#353)
    * [Modal] Refactor tests
    
    * Replacing name with id. Import a component theme
    
    * Fixed cases of closure of modal window
    
    * Fixed MaskedInput test
    
    Co-authored-by: MickRomanov <57683220+MickRomanov@users.noreply.github.com>

--- demo/cypress/Modal/index.tsx
+++ demo/cypress/Modal/index.tsx
@@ -5,22 +5,24 @@ import * as L from '../../../korus-ui';
 const Alerts = ({ setActiveAlertKey }: any) => (
   <>
     <L.ModalAlert
+      id="leave"
       alertKey="leave"
       onClose={() => setActiveAlertKey(null)}
     >
       <L.H2>Title!</L.H2>
       <L.P>Lorem sit amet!</L.P>
       <L.Div>
-        <L.Button onClick={() => setActiveAlertKey(null)}>
+        <L.Button id="cancelButton" onClick={() => setActiveAlertKey(null)}>
           Cancel
         </L.Button>
         &nbsp;&nbsp;&nbsp;
-        <L.Button _warning onClick={() => setActiveAlertKey(null)}>
+        <L.Button id="okButton" _warning onClick={() => setActiveAlertKey(null)}>
           Ok
         </L.Button>
       </L.Div>
     </L.ModalAlert>
     <L.ModalAlert
+      id="simple"
       alertKey="simple"
       onClose={() => setActiveAlertKey(null)}
     >
@@ -58,8 +60,8 @@ export const Modal = () => {
           </L.Button>
         </L.ModalBody>
         <L.ModalFooter>
-          <L.Button onClick={() => setOpen(false)}>Cancel</L.Button>
-          <L.Button _warning form="modal-form" onClick={() => setOpen(false)} onValidationFail={() => alert('Заполните все поля, пожалуйста!')}>Submit</L.Button>
+          <L.Button id="cancel" onClick={() => setOpen(false)}>Cancel</L.Button>
+          <L.Button id="submit" _warning form="modal-form" onClick={() => setOpen(false)} onValidationFail={() => alert('Заполните все поля, пожалуйста!')}>Submit</L.Button>
         </L.ModalFooter>
         <Alerts setActiveAlertKey={setActiveAlertKey} />
       </L.Modal>
