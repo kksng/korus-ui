4061dad54df6ce7862a7873fea9d836c0ef1764c
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-12 13:05:53 +0300

    FEND-1360 [NumericRange] Update documentation in Storybook (#366)
    * FEND-1360 [NumericRange] Update documentation in Storybook
    
    * Corrections based on comments

---
+++ storybook/storybook/stories/form/NumericRange/Examples/Limits.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import * as L from '@korus/leda';
+
+/* eslint-disable react/no-unescaped-entities */
+export const Limits = {
+  liveDemo: `
+const Limits = () => {
+  const [value, setValue] = React.useState([null, null]);
+
+  return (
+    <L.NumericRange
+      max={200}
+      min={0}
+      step={1}
+      placeholder="Gimme ur number!"
+    />
+  );
+};
+render(<Limits />);
+`,
+  text: (
+    <L.Div _block>
+      <L.P>
+        Для установки максимального и минимального значений для всего диапазона, передайте соответствующие значения в <b>max</b> и <b>min</b>.
+      </L.P>  
+    </L.Div>
+  ),
+};
