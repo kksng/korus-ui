aa770392fd5a81ebff5ded15797a535477242a13
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-18 10:25:44 +0300

    FEND-1363 [Password] Update documentation in Storybook (#374)

--- storybook/storybook/stories/form/Password/Examples/Customization.js
+++ storybook/storybook/stories/form/Password/Examples/Customization.js
@@ -1,19 +1,25 @@
 import * as L from '@korus/leda';
+import * as React from 'react';
 import { componentSrc } from './index';
 
-export const Api = {
+export const Customization = {
   liveDemo: `
-const Api = () => {
+const Customization = () => {
   const [value, setValue] = React.useState('');
   
   const handleChange = (ev) => {
     setValue(ev.component.value);
   };
-  
+
+  const inputRender = ({ elementProps, Element }) => (
+    <Element {...elementProps} style={{ border: '3px solid red' }} />
+  ); 
+
   return (
     <L.Password
       onChange={handleChange}
       value={value}
+      inputRender={inputRender}
       _width-30
       passwordEvaluators={[
         {
@@ -48,7 +54,25 @@ const Api = () => {
   );
 };
 
-render(<Api />);
+render(<Customization />);
 `,
+  text: (
+    <L.Div _block>
+      <L.P>
+        Для настройки внешнего вида частей компонента используйте методы с суффиксом <b>Render:</b>
+      </L.P>
+      <L.Ul _txt-list>
+        <L.Li>
+          <b>inputRender</b> - поле ввода
+        </L.Li>
+        <L.Li>
+          <b>passwordVisibilityRender</b> - настройка иконки видимости пароля
+        </L.Li>
+        <L.Li>
+          <b>wrapperRender</b> - корневой элемент, в который вложены все остальные<br />
+        </L.Li>
+      </L.Ul>
+    </L.Div>
+  ),
   source: componentSrc,
 };
