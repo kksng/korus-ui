a051e3cf1f3842af0d859202f1a9885e9c28228a
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-22 13:50:54 +0300

    FEND-1423 [Drawer] Added component to Storybook

---
+++ storybook/storybook/stories/layout/Drawer/Drawer.stories.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Api } from './Examples';
+import { propsDesc } from './propsDescription';
+import { ApiStory } from '../../../components/ApiStory';
+
+storiesOf('Layout|Drawer', module)
+  .add('API', context => (<ApiStory {...Api} customProps={propsDesc} context={context} />))
