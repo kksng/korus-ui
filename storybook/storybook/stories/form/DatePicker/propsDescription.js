5968b21fb736d623ded55e45a9a6752f6e9bee80
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-22 14:21:39 +0300

    Updated the list of props

--- storybook/storybook/stories/form/DatePicker/propsDescription.js
+++ storybook/storybook/stories/form/DatePicker/propsDescription.js
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import * as L from '@korus/leda';
 import { linkTo } from '@storybook/addon-links';
+import { RenderEvent } from '../../../propsHelpers';
+import {
+  form,
+  invalidMessage,
+  isRequired,
+  isValid,
+  name,
+  requiredMessage,
+  shouldValidateUnmounted,
+  validator,
+  invalidMessageRender,
+} from '../../basics/6_validation/propsDescription';
 
 /* eslint-disable max-len, react/no-unescaped-entities */
 export const propsDesc = [
   {
-    name: 'disabledDates',
-    type: '(Date | [Date, Date])[]',
-    required: false,
-    description: 'Даты, которые отключены для выбора.'
+    name: 'className',
+    type: 'string',
+    require: false,
+    description: 'Классы для компонента',
   },
   {
     name: (
@@ -22,6 +34,40 @@ export const propsDesc = [
     type: 'string',
     required: false,
     description: 'Формат даты. По умолчанию dd.MM.yyyy',
+  },
+  {
+    name: (
+      <L.A
+        onClick={linkTo('Form|DatePicker', 'Кастомизация')}
+        target="_self"
+      >
+        iconRender
+      </L.A>
+    ),
+    type: (
+      <L.Span>
+        <RenderEvent /> => React.ReactNode
+      </L.Span>
+    ),
+    required: false,
+    description: 'Кастомизация иконки календаря',
+  },
+  {
+    name: (
+      <L.A
+        onClick={linkTo('Form|DatePicker', 'Кастомизация')}
+        target="_self"
+      >
+        inputRender
+      </L.A>
+    ),
+    type: (
+      <L.Span>
+        <RenderEvent /> => React.ReactNode
+      </L.Span>
+    ),
+    required: false,
+    description: 'Кастомизация поля ввода',
   },
   {
     name: 'isDisabled',
@@ -299,4 +345,33 @@ interface DateTimeInputRefCurrent {
     required: false,
     description: 'Значение поля ввода',
   },
+  {
+    name: (
+      <L.A
+        onClick={linkTo('Form|DatePicker', 'Кастомизация')}
+        target="_self"
+      >
+        wrapperRender
+      </L.A>
+    ),
+    type: (
+      <L.Span>
+        <RenderEvent /> => React.ReactNode
+      </L.Span>
+    ),
+    required: false,
+    description: 'Кастомизация враппера компонента',
+  },
+];
+
+export const validationPropsDesc = [
+  form,
+  invalidMessage,
+  invalidMessageRender,
+  isRequired,
+  isValid,
+  name,
+  requiredMessage,
+  shouldValidateUnmounted,
+  validator,
 ];
