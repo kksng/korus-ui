f1eede3af2caf12d59b6bd0803aaf31983fe573c
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-24 10:47:18 +0300

    FEND-1433 [DateTimeRange] Update documentation in Storybook (#384)
    * FEND-1433 [DateTimeRange] Update documentation in Storybook
    
    * Update the list of props

--- storybook/storybook/stories/form/DateTimeRange/DateTimeRange.stories.js
+++ storybook/storybook/stories/form/DateTimeRange/DateTimeRange.stories.js
@@ -6,19 +6,23 @@ import
   BasicUsage,
   Format,
   MinMax,
-  TimeStep,
   UX,
 } from './Examples';
 import { Story } from '../../../components/Story';
 import { propsDesc } from './propsDescription';
 import { ApiStory } from '../../../components/ApiStory';
+import { calendarPropsDesc } from '../../../commonProps/CalendarProps';
+
+const compoundProps = [
+  { componentName: 'DateTimeRange', props: propsDesc },
+  { componentName: 'Calendar', props: calendarPropsDesc },
+];
 
 storiesOf('Form| DateTimeRange', module)
-  .add('API', context => (<ApiStory {...Api} customProps={propsDesc} context={context} />))
+  .add('API', context => (<ApiStory {...Api} compoundCustomProps={compoundProps} context={context} />))
   .add('Базовый пример', context => (<Story {...BasicUsage} customProps={propsDesc} context={context} />))
   .add('Ограничение выбора дат', context => (<Story {...MinMax} customProps={propsDesc} context={context} />))
   .add('UX', context => (<Story {...UX} customProps={propsDesc} context={context} />));
 
 storiesOf('Form| DateTimeRange.Props', module)
-  .add('format', context => (<Story {...Format} customProps={propsDesc} context={context} />))
-  .add('timeStepMinutes', context => (<Story {...TimeStep} customProps={propsDesc} context={context} />));
+  .add('format', context => (<Story {...Format} customProps={propsDesc} context={context} />));
