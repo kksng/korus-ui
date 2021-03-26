8ecbeffef6a9f82636acae49017fdd1177692fba
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-03-26 15:31:53 +0300

    FEND-1436 [Drawer] Tests for the component (#387)

--- demo/cypress/Drawer/index.tsx
+++ demo/cypress/Drawer/index.tsx
@@ -3,7 +3,8 @@ import * as L from '../../../korus-ui';
 
 export const Drawer = (): React.ReactElement => {
   return (
-    <L.Drawer id="drawer" position="right">
+    <>
+    <L.Drawer id="drawerRight" position="right">
       <L.Div style={{ width: 300, padding: 20 }}>
         <L.H3>Basic content</L.H3>
         <L.Input />
@@ -12,5 +13,15 @@ export const Drawer = (): React.ReactElement => {
         <L.Button _warning>Submit</L.Button>
       </L.Div>
     </L.Drawer>
+    <L.Drawer id="drawerLeft" position="left">
+      <L.Div style={{ width: 300, padding: 20 }}>
+        <L.H3>Some content</L.H3>
+        <L.Input />
+        <L.Input />
+        <L.Input />
+        <L.Button _warning>Submit</L.Button>
+      </L.Div>
+    </L.Drawer>
+    </>
   );
 };
