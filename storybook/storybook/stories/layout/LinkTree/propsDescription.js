d77ac5371837f196680547132ac12edd01230abe
Ksenia Lugovaya <lugovaya.ka@esphere.ru>
2021-03-29 09:28:59 +0300

    FEND-1424 [LinkTree] Added component to Storybook (#391)
    * FEND-1424 [LinkTree] Added component to Storybook
    
    * Fixed type

---
+++ storybook/storybook/stories/layout/LinkTree/propsDescription.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import * as L from '@korus/leda';
+
+export const propsDesc = [
+  {
+    name: 'data',
+    type: (
+      <>
+      {'('}
+      <L.Tooltip title={(
+        <pre>{`
+{
+  onClick?: () => void,
+  text: string,
+}
+        `}
+        </pre>
+      )}
+      >
+        <L.Span _txt-success>TerminalItem</L.Span>
+      </L.Tooltip>
+      {' | '}
+      <L.Tooltip title={(
+        <pre>{`
+{
+  [x: string]: (TerminalItem | InternalItem)[],
+}
+        `}
+        </pre>
+      )}
+      >
+        <L.Span _txt-success>InternalItem</L.Span>
+      </L.Tooltip>
+      {')[]'}
+      </>
+    ),
+    required: true,
+    description: 'Данные для дерева ссылок',
+  },
+  {
+    name: 'theme',
+    type: (
+      <L.Tooltip title={(
+        <pre>{`
+{
+  current:                  'current',
+  last:                     'last',
+  linkTreeIcon:             'link-tree-icon',
+  linkTreeIconExpanded:     'link-tree-icon-expanded',
+  linkTreeItem:             'link-tree-item',
+  linkTreeList:             'link-tree-list',
+  opened:                   'opened',
+  wrapper:                  'link-tree-wrapper',
+}
+        `}
+        </pre>
+      )}
+      >
+        <L.Span _txt-success>DefaultLinkTreeTheme</L.Span>
+      </L.Tooltip>
+    ),
+    required: false,
+    description: 'Тема компонента',
+  },
+];
