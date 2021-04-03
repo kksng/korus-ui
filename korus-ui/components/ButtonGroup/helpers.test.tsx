c9083897ff8efb1ea765197cdb3ee9b8e1f67b85
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-04-03 22:02:32 +0300

    Update jest tests

--- korus-ui/components/ButtonGroup/helpers.test.tsx
+++ korus-ui/components/ButtonGroup/helpers.test.tsx
@@ -1,28 +1,80 @@
 import * as helpers from './helpers';
 
 describe('getArrayValue tests', () => {
-  it('Numbers', () => {
-    const value = [1, 2];
-    const item = 1;
-    const currentItem = 2;
-    expect(helpers.getArrayValue(value, item)).not.toEqual(currentItem);
+  describe('Adding new values to an array', () => {
+    it('Array of numbers', () => {
+      const value = [1, 2, 3];
+      const currentItem = 4;
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = [1, 2, 3, 4];
+      expect(received).toEqual(expected);
+    });
+
+    it('Adding object into array of numbers', () => {
+      const value = [1, 2, 3];
+      const currentItem = { object: 1 };
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = [1, 2, 3, { object: 1 }];
+      expect(received).toEqual(expected);
+    });
+
+    it('Adding string into array of numbers', () => {
+      const value = [1, 2, 3];
+      const currentItem = 'str1';
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = [1, 2, 3, 'str1'];
+      expect(received).toEqual(expected);
+    });
+
+    it('Array of strings', () => {
+      const value = ['str1', 'str2', 'str3'];
+      const currentItem = 'str4';
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = ['str1', 'str2', 'str3', 'str4'];
+      expect(received).toEqual(expected);
+    });
+
+    it('Adding number into array of strings', () => {
+      const value = ['str1', 'str2', 'str3'];
+      const currentItem = 1;
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = ['str1', 'str2', 'str3', 1];
+      expect(received).toEqual(expected);
+    });
+
+    it('Adding object into array of strings', () => {
+      const value = ['str1', 'str2', 'str3'];
+      const currentItem = { object: 1 };
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = ['str1', 'str2', 'str3', { object: 1 }];
+      expect(received).toEqual(expected);
+    });
+
+    it('Array of undefined', () => {
+      const value = [undefined, undefined];
+      const currentItem = { object: 2 };
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = [undefined, undefined, { object: 2 }];
+      expect(received).toEqual(expected);
+    });
   });
 
-  it('Strings', () => {
-    const value = ['string1', 'string2'];
-    const item = 'string1';
-    const currentItem = 'string2';
-    expect(helpers.getArrayValue(value, item)).not.toEqual(currentItem);
-  });
+  describe('Deleting values from an array', () => {
+    it('Array of numbers', () => {
+      const value = [1, 2, 3];
+      const currentItem = 2;
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = [1, 3];
+      expect(received).toEqual(expected);
+    });
 
-  it('Objects', () => {
-    const value = [
-      { object: 1 },
-      { object: 2 },
-    ];
-    const item = { object: 1 };
-    const currentItem = { object: 2 };
-    expect(helpers.getArrayValue(value, item)).not.toEqual(currentItem);
+    it('Array of strings', () => {
+      const value = ['str1', 'str2', 'str3'];
+      const currentItem = 'str1';
+      const received = helpers.getArrayValue(value, currentItem);
+      const expected = ['str2', 'str3'];
+      expect(received).toEqual(expected);
+    });
   });
 });
 
@@ -31,32 +83,34 @@ describe('compareItems tests', () => {
     const value = 9;
     const item = 9;
     const currentItem = 1;
-    expect(helpers.compareItems(value, currentItem)).toEqual(false);
-    expect(helpers.compareItems(value, item)).toEqual(true);
+    expect(helpers.compareItems(value, currentItem)).toBe(false);
+    expect(helpers.compareItems(value, item)).toBe(true);
   });
 
   it('Objects', () => {
     const value = { key: 9 };
     const item = { key: 9 };
     const textField = 'key';
-    expect(helpers.compareItems((value[textField]), (item[textField]))).toEqual(true);
+    expect(helpers.compareItems(value[textField], item[textField])).toEqual(
+      true
+    );
   });
 
   it('Strings', () => {
     const value = 'string1';
     const currentItem = 'string2';
-    expect(helpers.compareItems(value, currentItem)).toEqual(false);
+    expect(helpers.compareItems(value, currentItem)).toBe(false);
   });
 
   it('Value null', () => {
     const value = null;
     const currentItem = 1;
-    expect(helpers.compareItems(value, currentItem)).toEqual(false);
+    expect(helpers.compareItems(value, currentItem)).toBe(false);
   });
 
   it('Value undefined', () => {
     const value = undefined;
     const currentItem = '1';
-    expect(helpers.compareItems(value, currentItem)).toEqual(false);
+    expect(helpers.compareItems(value, currentItem)).toBe(false);
   });
 });
