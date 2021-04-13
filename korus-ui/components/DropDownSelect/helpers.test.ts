dac1d12a325f790d80186fee05e79a3a0f0b5955
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-04-13 15:14:55 +0300

    Update jest tests

--- korus-ui/components/DropDownSelect/helpers.test.ts
+++ korus-ui/components/DropDownSelect/helpers.test.ts
@@ -1,5 +1,173 @@
 // @ts-nocheck
+import { defaultDropDownSelectTheme as theme } from './theme';
 import * as helpers from './helpers';
+
+describe('getComponentClassNames tests', () => {
+  it('Should return .dropdownselect-input-wrapper, .dropdownselect-select-icon .dropdownselect-wrapper and added .opened and .danger', () => {
+    const data = {
+      isDisabled: false,
+      isFocused: false,
+      isOpen: true,
+      isRequired: false,
+      isValid: false,
+      theme,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperInvalid}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconOpened}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should return .dropdownselect-input-wrapper, .dropdownselect-select-icon .dropdownselect-wrapper and added .opened and .required', () => {
+    const data = {
+      isDisabled: false,
+      isOpen: true,
+      isRequired: true,
+      isValid: true,
+      theme,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperRequired}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconOpened}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should return .dropdownselect-input-wrapper, .dropdownselect-select-icon .dropdownselect-wrapper and added .closed and .required', () => {
+    const data = {
+      isDisabled: false,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperRequired}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should return .dropdownselect-input-wrapper, .dropdownselect-select-icon .dropdownselect-wrapper and added .focused, .required and .closed', () => {
+    const data = {
+      isDisabled: false,
+      isFocused: true,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperFocused} ${theme.inputWrapperRequired}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should add a class name if className is string', () => {
+    const data = {
+      className: 'customClass',
+      isDisabled: false,
+      isFocused: true,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperFocused} ${theme.inputWrapperRequired}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper} customClass`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Shouldn`t add a class name if className is undefined', () => {
+    const data = {
+      className: undefined,
+      isDisabled: false,
+      isFocused: true,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperFocused} ${theme.inputWrapperRequired}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should remove class "required" is value is string', () => {
+    const data = {
+      isDisabled: false,
+      isFocused: true,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+      value: 'componentValue',
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperFocused}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should remove class "required" is value is number', () => {
+    const data = {
+      isDisabled: false,
+      isFocused: true,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+      value: 999,
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperFocused}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+
+  it('Should remove class "required" is value is object', () => {
+    const data = {
+      isDisabled: false,
+      isFocused: true,
+      isOpen: false,
+      isRequired: true,
+      isValid: true,
+      theme,
+      value: { value: 'componentValue' },
+    };
+    const expected = {
+      inputWrapperClassNames: `${theme.inputWrapper} ${theme.inputWrapperFocused}`,
+      selectIconClassNames: `${theme.selectIcon} ${theme.selectIconClosed}`,
+      wrapperClassNames: `${theme.wrapper}`,
+    };
+    const received = helpers.getComponentClassNames(data);
+    expect(received).toEqual(expected);
+  });
+});
 
 describe('getInputValue tests', () => {
   it('Should return value if filterValue is string', () => {
@@ -42,6 +210,14 @@ describe('getInputValue tests', () => {
     expect(received).toBe(expected);
   });
 
+  it('Should return empty string if value is null', () => {
+    const value = null;
+    const filterValue = '';
+    const expected = '';
+    const received = helpers.getInputValue(value, filterValue);
+    expect(received).toBe(expected);
+  });
+
   it('Should return string if filtervalue is empty string and value is number', () => {
     const value = 100500;
     const filterValue = '';
@@ -50,16 +226,52 @@ describe('getInputValue tests', () => {
     expect(received).toBe(expected);
   });
 
-  // it('', () => {
-  //   const value = {
-  //     city: 'Stuttgard',
-  //     city1: 'Berlin',
-  //   };
-  //   const textField = 'city';
-  //   const expected = 'Stuttgard';
-  //   const received = helpers.getInputValue(value, textField);
-  //   expect(received).toBe(expected);
-  // });
+  it('Should return string if filtervalue is null and value is number', () => {
+    const value = 100500;
+    const filterValue = null;
+    const expected = '100500';
+    const received = helpers.getInputValue(value, filterValue);
+    expect(received).toBe(expected);
+  });
+
+  it('Should return value by textField if filterValue null and value is object', () => {
+    const value = {
+      city: 'Stuttgard',
+      country: 'Germany',
+    };
+    const textField = 'city';
+    const filterValue = null;
+    const shouldFilterValues = true;
+    const expected = 'Stuttgard';
+    const received = helpers.getInputValue(value, filterValue, shouldFilterValues, textField);
+    expect(received).toBe(expected);
+  });
+
+  it('Should return filterValue if filterValue string and value is object', () => {
+    const value = {
+      city: 'Stuttgard',
+      country: 'Germany',
+    };
+    const textField = 'country';
+    const filterValue = 'Ger';
+    const shouldFilterValues = true;
+    const expected = 'Ger';
+    const received = helpers.getInputValue(value, filterValue, shouldFilterValues, textField);
+    expect(received).toBe(expected);
+  });
+
+  it('Should return filterValue if textField empty string and value is object', () => {
+    const value = {
+      city: '',
+      country: 'Germany',
+    };
+    const textField = '';
+    const filterValue = 'Ger';
+    const shouldFilterValues = true;
+    const expected = 'Ger';
+    const received = helpers.getInputValue(value, filterValue, shouldFilterValues, textField);
+    expect(received).toBe(expected);
+  });
 });
 
 describe('DropDownSelect filterData', () => {
