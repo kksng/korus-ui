77dab79e94cd7dce04a8a9af2922f5af8e9dd536
MickRomanov <57683220+MickRomanov@users.noreply.github.com>
2021-04-19 15:56:31 +0300

    END-1499 [Currency] Update documentation in Storybook (#411)
    * FEND-1499 [Currency] Update documentation in Storybook
    
    * Added missing validators
    
    * Added tests for missing validators
    
    * Delete missing validators and tests
    
    * Fixed id
    
    * Delete RUB folder

--- storybook/storybook/stories/layout/Currency/Currency.stories.js
+++ storybook/storybook/stories/layout/Currency/Currency.stories.js
@@ -4,9 +4,12 @@ import { storiesOf } from '@storybook/react';
 import
 {
   Api,
+  Customization,
 } from './Examples';
 import { propsDesc } from './propsDescription';
+import { Story } from '../../../components/Story';
 import { ApiStory } from '../../../components/ApiStory';
 
 storiesOf('Layout|Currency', module)
   .add('API', context => (<ApiStory {...Api} customProps={propsDesc} context={context} />))
+  .add('Кастомизация', context => (<Story {...Customization} customProps={propsDesc} context={context} />))
